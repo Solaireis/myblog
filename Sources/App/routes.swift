164b92454657fee3b98f24ed333c787f8615db68
Eden <64953425+Solaireis@users.noreply.github.com>
2024-06-14 03:57:03 +0800

    learning dynamic routing w vapor

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,6 +8,10 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    app.get("hello", ":name") { req -> String in
+        let name = req.parameters.get("name")!
+        return "Hello, \(name)!"
+    }
     app.get("bye") { req async -> String in
         "Bye Bye!"
     }
